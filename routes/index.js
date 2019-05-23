ed99fcc759fefdc1058235ffd7984b3325b8ea39
Rahul <rahul@pixlcoders.com>
2019-05-23 18:54:10 +0530

    API's done for company, benefits table

--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,8 @@
 const AuthRouter   = require('./auth.route');
 const UserRouter   = require('./user.route');
 const QaJobsRouter = require('./qa_jobs.route');
+const CompaniesRouter = require('./companies.route');
+const BenefitsRouter = require('./benefits.route');
 
 const prefix = '/api';
 
@@ -10,4 +12,7 @@ module.exports = (app) => {
     app.use(`${prefix}/auth`, AuthRouter);
     app.use(`${prefix}/user`, UserRouter);
     app.use(`${prefix}/qa-jobs`, QaJobsRouter);
+    app.use(`${prefix}/companies`, CompaniesRouter);
+    app.use(`${prefix}/benefits`, BenefitsRouter);
 };
+'use strict';
