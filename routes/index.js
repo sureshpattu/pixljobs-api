654c5a587874865d76d390963b730d3bd958ba8e
Naren <naren@pixlcoders.com>
2019-05-23 18:53:08 +0530

    api written for notifications,admin-notifications,tags

--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,9 @@
 const AuthRouter   = require('./auth.route');
 const UserRouter   = require('./user.route');
 const QaJobsRouter = require('./qa_jobs.route');
+const NotificationsRouter = require('./notifications.route');
+const AdminNotificationsRouter = require('./admin_notifications.route');
+const TagsRouter = require('./tags.route');
 
 const prefix = '/api';
 
@@ -10,4 +13,7 @@ module.exports = (app) => {
     app.use(`${prefix}/auth`, AuthRouter);
     app.use(`${prefix}/user`, UserRouter);
     app.use(`${prefix}/qa-jobs`, QaJobsRouter);
+    app.use(`${prefix}/notifications`, NotificationsRouter);
+    app.use(`${prefix}/admin-notifications`, AdminNotificationsRouter);
+    app.use(`${prefix}/tags`, TagsRouter);
 };
