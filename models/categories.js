d118474680769035a0b08ba3c2156bf8c4a3b8c2
Deepak <deepak@pixlcoders.com>
2019-05-23 18:49:48 +0530

    api written for category,technology,requirement,job_aplication is done

---
+++ models/categories.js
@@ -0,0 +1,24 @@
+'use strict';
+
+module.exports = function(sequelize, Sequelize) {
+
+    return sequelize.define('categories', {
+        id  :{
+            type        :Sequelize.UUID,
+            primaryKey  :true,
+            defaultValue:Sequelize.UUIDV4,
+            allowNull   :false
+        },
+        name:{
+            type:Sequelize.TEXT('long')
+        },
+        desc:{
+            type:Sequelize.TEXT('long')
+        }
+    }, {
+        underscored:true,
+        version    :true,
+        deletedAt  :'destroyTime',
+        paranoid   :true
+    });
+};
--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,14 @@
 'use strict';
 
-const AuthRouter   = require('./auth.route');
-const UserRouter   = require('./user.route');
-const QaJobsRouter = require('./qa_jobs.route');
+const AuthRouter            = require('./auth.route');
+const UserRouter            = require('./user.route');
+const QaJobsRouter          = require('./qa_jobs.route');
+const CategoriesRouter      = require('./categories.route');
+const RequirementsRouter    = require('./requirements.route');
+const TechnologiesRouter    = require('./technologies.route');
+const JobApplicationsRouter = require('./job_applications.route');
+const JobCategoriesRouter   = require('./job_categories.route');
+const JobRequirementsRouter = require('./job_requirements.route');
 
 const prefix = '/api';
 
@@ -10,4 +16,10 @@ module.exports = (app) => {
     app.use(`${prefix}/auth`, AuthRouter);
     app.use(`${prefix}/user`, UserRouter);
     app.use(`${prefix}/qa-jobs`, QaJobsRouter);
+    app.use(`${prefix}/categories`, CategoriesRouter);
+    app.use(`${prefix}/requirements`, RequirementsRouter);
+    app.use(`${prefix}/technologies`, TechnologiesRouter);
+    app.use(`${prefix}/job-applications`, JobApplicationsRouter);
+    app.use(`${prefix}/job-categories`, JobCategoriesRouter);
+    app.use(`${prefix}/qa-requirements`, JobRequirementsRouter);
 };
