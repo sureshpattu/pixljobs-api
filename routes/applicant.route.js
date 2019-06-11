928ffee8bc8903036a447a02cc3ab5020f58fff4
Sunil <sunil@pixlcoders.com>
2019-06-11 19:33:12 +0530

    recruiter apis added

--- routes/applicant.route.js
+++ routes/applicant.route.js
@@ -8,7 +8,6 @@ const Controller = require('../controllers/applicant.controller');
 router
     .get('/:id', (req, res) => { Controller.read(req, res); })
     .post('/reset-password/:id', (req, res) => { Controller.resetPassword(req, res); })
-    .post('/reset-passcode/:id', (req, res) => { Controller.resetPassCode(req, res); })
     .post('/avatar/upload/:id', (req, res) => { Controller.photo(req, res); })
     .get('/photo/:image', (req, res) => { Controller.viewPhoto(req, res); })
     .put('/:id', (req, res) => { Controller.update(req, res) });
