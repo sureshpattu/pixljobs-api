da7919bb0726cfc9b22e9461c5e3281bbf932164
Deepak <deepak@pixlcoders.com>
2019-06-17 10:37:18 +0530

    api changes done

--- routes/applicant.route.js
+++ routes/applicant.route.js
@@ -8,7 +8,7 @@ const Controller = require('../controllers/applicant.controller');
 router
     .get('/:id', (req, res) => { Controller.read(req, res); })
     .post('/reset-password/:id', (req, res) => { Controller.resetPassword(req, res); })
-    .post('/avatar/upload/:id', (req, res) => { Controller.photo(req, res); })
+    .post('/avatar/upload', (req, res) => { Controller.photo(req, res); })
     .get('/photo/:image', (req, res) => { Controller.viewPhoto(req, res); })
     .put('/:id', (req, res) => { Controller.update(req, res) });
 
