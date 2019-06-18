f124ed6b3e172723ad1ce68fe8d94a28fed644fc
Suresh Pattu <sureshpattu2002@gmail.com>
2019-06-18 17:07:41 +0530

    Job table extra params added

--- models/companies.js
+++ models/companies.js
@@ -1,43 +1,101 @@
 'use strict';
 
 module.exports = function(sequelize, Sequelize) {
-
     return sequelize.define('companies', {
-        id      :{
+        id           :{
             type        :Sequelize.UUID,
             primaryKey  :true,
             defaultValue:Sequelize.UUIDV4,
             allowNull   :false
         },
-        user_id:{
+        user_id      :{
             type:Sequelize.UUID
         },
-        name    :{
+        name         :{
             type:Sequelize.TEXT('long')
         },
-        industry:{
+        industry     :{
             type:Sequelize.TEXT('long')
         },
-        size    :{
+        size         :{
             type:Sequelize.STRING
         },
-        about   :{
+        about        :{
             type:Sequelize.TEXT('long')
         },
-        email   :{
-            type    :Sequelize.STRING,
-            validate:{
-                isEmail:true
-            },
-            unique  :{
-                args:true,
-                msg :'Email address already in use!'
-            }
-        },
-        logo    :{
+        phone_code   :{
             type:Sequelize.STRING
         },
-        status  :{
+        phone_number :{
+            type:Sequelize.STRING
+        },
+        mobile_code  :{
+            type:Sequelize.STRING
+        },
+        mobile_number:{
+            type:Sequelize.STRING
+        },
+        street       :{
+            type:Sequelize.STRING
+        },
+        area         :{
+            type:Sequelize.STRING
+        },
+        locality     :{
+            type:Sequelize.STRING
+        },
+        city         :{
+            type:Sequelize.STRING
+        },
+        state        :{
+            type:Sequelize.STRING
+        },
+        pin          :{
+            type:Sequelize.STRING
+        },
+        country      :{
+            type:Sequelize.STRING
+        },
+        email        :{
+            type:Sequelize.STRING
+        },
+        logo         :{
+            type:Sequelize.STRING
+        },
+        website         :{
+            type:Sequelize.STRING
+        },
+        pan        :{
+            type:Sequelize.STRING
+        },
+        tan         :{
+            type:Sequelize.STRING
+        },
+        gst         :{
+            type:Sequelize.STRING
+        },
+        cin         :{
+            type:Sequelize.STRING
+        },
+        photo_1      :{
+            type:Sequelize.STRING
+        },
+        photo_2      :{
+            type:Sequelize.STRING
+        },
+        photo_3      :{
+            type:Sequelize.STRING
+        },
+        photo_4      :{
+            type:Sequelize.STRING
+        },
+        video_1      :{
+            type:Sequelize.STRING
+        },
+        video_2      :{
+            type:Sequelize.STRING
+        },
+        status       :{
             type        :Sequelize.ENUM('closed', 'active', 'review', 'deleted'),
             defaultValue:'review'
         }
