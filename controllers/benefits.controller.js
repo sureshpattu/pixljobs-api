8218053d1cfd35c1d759e91e8011b7593e4a67e6
Sunil <sunil@pixlcoders.com>
2019-07-03 13:25:37 +0530

    benefits : create benefits api updated

--- controllers/benefits.controller.js
+++ controllers/benefits.controller.js
@@ -17,6 +17,51 @@ function fetchSingle(_id, res) {
     });
 }
 
+function createBenefits(req, res, _company_id) {
+    waterfall(req.body.benefits.map(function(_obj) {
+        return function(lastItemResult, CB) {
+            if(!CB) {
+                CB             = lastItemResult;
+                lastItemResult = null;
+            }
+            let _query      = {};
+            _query[Op.or]   = [];
+            let lookupValue = _obj.toLowerCase();
+            _query[Op.or].push({
+                id:sequelize.where(sequelize.fn('LOWER', db.sequelize.col('id')), 'LIKE',
+                    '%' + lookupValue + '%')
+            });
+            _query[Op.or].push({
+                name:sequelize.where(sequelize.fn('LOWER', db.sequelize.col('name')), 'LIKE',
+                    '%' + lookupValue + '%')
+            });
+            Model.findOne({where:_query}).then((_data) => {
+                if(!_data) {
+                    Model.create({name:_obj}).then((_data) => {
+                        CompanyBenefits.create({company_id:_company_id, benefit_id:_data.id}).then((_data) => {
+                            CB(null, []);
+                        }).catch(_err => {
+                            ApiHelpers.error(res, _err);
+                        });
+                    }).catch(_err => {
+                        ApiHelpers.error(res, _err);
+                    });
+                } else {
+                    CompanyBenefits.create({company_id:_company_id, benefit_id:_data.id}).then((_data) => {
+                        CB(null, []);
+                    }).catch(_err => {
+                        ApiHelpers.error(res, _err);
+                    });
+                }
+            }).catch(_err => {
+                CB(null, []);
+            });
+        };
+    }), function() {
+        ApiHelpers.success(res, null, 'success');
+    });
+}
+
 module.exports = {
     index:(req, res) => {
         Model.findAll({where:{}}).then((_data) => {
@@ -35,50 +80,22 @@ module.exports = {
             return ApiHelpers.error(res, true, 'Parameters missing');
         }
 
-
         let _company_id = req.body.company_id;
-
-        waterfall(req.body.benefits.map(function(_obj) {
-            return function(lastItemResult, CB) {
-                if(!CB) {
-                    CB             = lastItemResult;
-                    lastItemResult = null;
-                }
-                let _query      = {};
-                _query[Op.or]   = [];
-                let lookupValue = _obj.toLowerCase();
-                _query[Op.or].push({
-                    id:sequelize.where(sequelize.fn('LOWER', db.sequelize.col('id')), 'LIKE',
-                        '%' + lookupValue + '%')
-                });
-                _query[Op.or].push({
-                    name:sequelize.where(sequelize.fn('LOWER', db.sequelize.col('name')), 'LIKE',
-                        '%' + lookupValue + '%')
-                });
-                Model.findOne({where:_query}).then((_data) => {
-                    if(!_data) {
-                        Model.create({name:_obj}).then((_data) => {
-                            CompanyBenefits.create({company_id:_company_id, benefit_id:_data.id}).then((_data) => {
-                                CB(null, []);
-                            }).catch(_err => {
-                                ApiHelpers.error(res, _err);
-                            });
-                        }).catch(_err => {
-                            ApiHelpers.error(res, _err);
-                        });
-                    } else {
-                        CompanyBenefits.create({company_id:_company_id, benefit_id:_data.id}).then((_data) => {
-                            CB(null, []);
-                        }).catch(_err => {
-                            ApiHelpers.error(res, _err);
-                        });
-                    }
+        CompanyBenefits.findOne({
+            where:{company_id:_company_id}
+        }).then((_compBen) => {
+            if(!_compBen) {
+                createBenefits(req, res, _company_id);
+            } else {
+                CompanyBenefits.destroy({where:{company_id:_company_id}}).then((_data) => {
+                    createBenefits(req, res, _company_id);
                 }).catch(_err => {
-                    CB(null, []);
+                    ApiHelpers.error(res, _err);
                 });
-            };
-        }), function() {
-            ApiHelpers.success(res, null, 'success');
+            }
+
+        }).catch(_err => {
+            ApiHelpers.error(res, _err);
         });
     },
 
