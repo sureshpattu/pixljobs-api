8c09767519145303f93ff54f7c1f75d1fccce0c2
Deepak <deepak@pixlcoders.com>
2019-07-04 15:44:11 +0530

    Change email for recruiter and applicant is done

--- routes/recruiter.route.js
+++ routes/recruiter.route.js
@@ -22,6 +22,7 @@ router
     .get('/:id', (req, res) => { Controller.read(req, res); })
     .get('/fetch-full/:id', (req, res) => { Controller.fetchFull(req, res); })
     .post('/reset-password/:id', (req, res) => { Controller.resetPassword(req, res); })
+    .post('/change-email/:id', (req, res) => { Controller.changeEmail(req, res); })
     .post('/photo/upload/:id', upload.single('photo'), (req, res) => { Controller.photo(req, res); })
     .get('/photo/:image', (req, res) => { Controller.viewPhoto(req, res); })
     .delete('/photo/:image', (req, res) => { Controller.removePhoto(req, res); })
